e34c68f45ab5d26af473bc633bde229e17003f0e
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-08 16:25:49 +0900

    added test_TreeNode_select_sub_node()

--- ch01/test_TreeMap.py
+++ ch01/test_TreeMap.py
@@ -16,3 +16,20 @@ def test_TreeNode_constructor(TreeNode):
     assert 'value' == result.itsValue, result.itsValue
     assert result.nodes[result.LESS] is None
     assert result.nodes[result.GREATER] is None
+
+
+def test_TreeNode_select_sub_node(TreeNode):
+    top = TreeNode
+    top.itsKey = 0
+    top.nodes[top.LESS] = TreeMap.TreeNode(-1, 'less')
+    top.nodes[top.GREATER] = TreeMap.TreeNode(1, 'greater')
+
+    result_index_less = top.selectSubNode(-1)
+    assert top.LESS == result_index_less, result_index_less
+    result_index_greater = top.selectSubNode(1)
+    assert top.GREATER == result_index_greater, result_index_greater
+
+    try:
+        result_raise = top.selectSubNode(0)
+    except AssertionError:
+        pass
